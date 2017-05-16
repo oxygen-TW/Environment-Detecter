c2edd07e25f5148a1301c7965d56c614b77f701f
oxygen-TW <howard2805@gmail.com>
2017-05-16 17:26:06 +0800

    新增USB分支，並加入修改成USB版的的IR人體紅外線感測器韌體

--- human_IR-detected_light/human_IR-detected_light.ino
+++ human_IR-detected_light/human_IR-detected_light.ino
@@ -1,45 +1,47 @@
-
-int sensor = 4;
-int sw = 7;
-int relay = 2; //繼電器
+/*
+   接腳定義
+   繼電器 數位2
+   人體紅外線感測器 數位4
+   Vcc = 5V
+   Gnd = 0V
+*/
+const int BaulRate = 9600;
+const int sensor = 4;
+const int relay = 2; //繼電器
 bool led = false;
 int Value = 0;
 
-bool sw_chk()
-{
-  if (digitalRead(sw) == HIGH)
-    return true;
-  else
-    return false;
-}
 
 void setup() {
   pinMode(relay, OUTPUT);
-  pinMode(sensor, INPUT); 
-  pinMode(sw, INPUT);
+  pinMode(sensor, INPUT);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  
-  Value = digitalRead(sensor);
-  Serial.println(Value);
 
-  if (Value == HIGH || sw_chk())
+  Value = digitalRead(sensor);
+
+  if (Value == HIGH)
   {
+     Serial.print("true");
+     
     if (led == false)
     {
       digitalWrite(relay, HIGH);
       led = true;
-      delay(10000);
     }
+   delay(100);
   }
   else if (Value == LOW)
   {
+    Serial.print("false");
     if (led == true)
     {
       digitalWrite(relay, LOW);
       led = false;
     }
+    delay(100);
   }
 }
