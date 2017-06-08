aacc848c1ad90ec39bbbeb6643503e04e94281a2
oxygen-TW <howard2805@gmail.com>
2017-06-08 18:21:02 +0800

    新增序列部讀取程式雛形(未測試)

---
+++ Serial_Read/Serial_Read_Tools/Serial_Read_Tools/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ReceiveSerialData("COM4", 9600, 8)
+    End Sub
+End Class
---
+++ Serial_Read/Serial_Read_Tools/Serial_Read_Tools/Serial_Read.vb
@@ -0,0 +1,31 @@
+﻿Module Serial_Read
+    Function ReceiveSerialData(ByVal _SerialPort, ByVal _BaudRate, ByVal _DataBits) As String
+        ' Receive strings from a serial port.
+        Dim returnStr As String = ""
+
+        Dim Serial_Object As IO.Ports.SerialPort = Nothing
+
+        Try
+            Serial_Object = My.Computer.Ports.OpenSerialPort(_SerialPort)
+            Serial_Object.BaudRate = _BaudRate
+            Serial_Object.DataBits = _DataBits
+            Serial_Object.ReadTimeout = 10000
+            Do
+                Dim Incoming As String = Serial_Object.ReadLine()
+                If Incoming Is Nothing Then
+                    Exit Do
+                Else
+                    returnStr &= Incoming & vbCrLf
+                End If
+            Loop
+        Catch ex_timeout As TimeoutException
+            returnStr = "Error: Serial Port read timed out."
+        Catch ex As Exception
+            returnStr = "Error: Can't open Serial port or other error"
+        Finally
+            If Serial_Object IsNot Nothing Then Serial_Object.Close()
+        End Try
+
+        Return returnStr
+    End Function
+End Module
